07959edede709ab2a6f9312dc21066d325868353
Jeffrey <jb6927@gmail.com>
2023-01-19 12:50:18 -0500

    moved town() into the functions.py for consistency
    changes the input() functions to not convert to int, instead the choices now just look for strings. added else statements to error handle bad inputs. believe will catch all bad inputs now.

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
 from pkg.functions import *
 
-
-# TODO  error handling for bad inputs
 # TODO  OPTIONAL - use some switch statements instead of if/else
 # TODO  game balance - add character buffs when beating dungeons so that you likely need to beat Diablo last
-
-def town(character):
-    while True:
-        if character.hp <= 0:
-            print('You have died! Game over!')
-            print('---------------------- \n')
-            break
-        elif not available_dungeons:
-            print('Congratulations! You have defeated all the enemies and won the game!')
-            break
-        else:
-            print('\n--------------------')
-            print('You are in town')
-            print('--------------------')
-            print('1) Enter a dungeon')
-            print('2) Chat with townsfolk')
-            print('3) Drink Mystery Potion')
-            print('4) Inspect your character')
-            print('--------------------')
-            choice = int(input('What would you like to do? '))
-            print('--------------------')
-            if choice == 1:
-                dungeon = choose_dungeon()
-                encounter(character, dungeon)
-                # del available_dungeons[dungeon]
-            elif choice == 2:
-                chat_townsfolk()
-            elif choice == 3:
-                drink_potion(character)
-            elif choice == 4:
-                character.character_screen()
-            else:
-                print('INVALID CHOICE. TRY AGAIN.')
+# TODO  FUTURE - add random loot when bosses get defeated, maybe add equipment, use SQL tables?
 
 
 while True:
